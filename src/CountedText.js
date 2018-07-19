a7e7a732a28122e0aa8b2fc18c6d4b0370ce55d7
Charles Martin <chasrmartin@gmail.com>
2018-07-19 15:39:38 -0600

    React components work

--- src/CountedText.js
+++ src/CountedText.js
@@ -1,12 +1,13 @@
 // -*-mode: rjsx -*-
 import React, { Component } from 'react';
 import Countable from 'countable';
+import {Line, Circle} from 'rc-progress';
 
 class Text extends Component {
     render() {
         return (
             <textarea id="words"
-                      cols="60" rows="25">
+                      cols="60" rows="10">
             </textarea>
         );
     }
@@ -14,7 +15,7 @@ class Text extends Component {
 
 class WordCount extends Component {
     render() {
-        console.log("Render called in WordCount");
+        console.log("WordCount.render called");
         console.log(this.props);
         return (
             <div>
@@ -24,19 +25,38 @@ class WordCount extends Component {
     }
 }
 
-class ProgressBar extends Component {
+class Progress extends Component {
+    constructor(props){
+        super(props);
+        console.log("Progress.ctor called");
+    }
+
     render() {
-        if(this.props.count < 750){
-            return (
-                <div>
-                  {this.props.count / 750}
-                </div>
-            );
-        } else {
-            return (
-                <h3>DONE!</h3>
-            );
-        }
+        console.log(`Progress.render called:`, this.props.percent);
+        const containerStyle = {
+            width: '250px',
+            display: 'inline-block'
+        };
+        const circleContainerStyle = {
+            width: '30px',
+            height: '30px',
+            display: 'inline-block',
+        };
+        return (
+            <div>
+              <div style={containerStyle}>
+                <Line percent={this.props.percent}
+                      strokeWidth="5"
+                      strokeColor={this.props.color} />
+              </div>
+              <br />
+              <div style={circleContainerStyle}>
+                <Circle percent={this.props.percent}
+                        strokeWidth="5"
+                        strokeColor={this.props.color}/>
+              </div> 
+            </div>
+        );
     }
 }
 
@@ -45,23 +65,35 @@ export default class CountedText extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
-        } ;
+            target: 750,
+            count: 0,
+            percent: 0,
+            color: '#3FC7FA',
+        };
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount(){
         let textArea = document.getElementById("words");
         Countable.on(textArea,
-                     count => this.setState({ count: count.words}));
+                     count => this.handleChange(count.words));
     }
-    
+    handleChange(count){
+        let pct = ((count / this.state.target)*100).toFixed(0);
+        this.setState({
+            count: count,
+            percent: pct,
+            color: (pct<=100) ? '#3FC7FA' : '#00FF00'
+        });
+
+    }
     render() {
         return (
             <div>
               <h2>I'm here!</h2>
               <Text/>
-              <WordCount count={this.state.count} />
-              <ProgressBar count={this.state.count} />
+              <WordCount count={this.state.count}  />
+              <Progress percent={this.state.percent} color={this.state.color}/>
             </div>
         );
     }
